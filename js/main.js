24b7e1058450f4ee3d30d0ea561f3603b8662032
kesevan626 <87601398+kesevan626@users.noreply.github.com>
2022-10-04 14:30:13 -0700

    changed font

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const navbar = document.querySelector(".nav-bar");
 const nav = document.querySelector("nav");
-const projectImage = document.querySelectorAll(".project-photo photo");
+const projectImage = document.querySelector(".project-photo photo");
 const hamburger = document.querySelector(".hamburger");
 const hero_p = document.querySelector(".hero-content p");
 const hero_big = document.querySelector(".hero-content h3");
 const txt = document.querySelector(".text");
-const messageBtn = document.querySelector(".message-btn");
+const messageBtn = document.querySelectorAll(".message-btn");
 const closeBtn = document.querySelector(".btn-close");
 const formContainer = document.querySelector(".form-container");
 
@@ -35,17 +35,20 @@ hamburger.addEventListener('click', (e)=>{
 closeBtn.addEventListener('click', ()=>{
   formContainer.style.transition = 'all 2s ease'
   formContainer.style.transform = 'translateY(-110%)'
-  messageBtn.style.transform = 'translateX(0%)'
-  messageBtn.style.opacity = 1
+  messageBtn.forEach((btn)=>{
+    btn.style.transform = 'translateX(0%)'
+    btn.style.opacity = 1
+  })
 })
-messageBtn.addEventListener('click',()=>{
-  messageBtn.style.transition = 'all 2s ease'
-  messageBtn.style.transform = 'translateX(-250%)'
-  messageBtn.style.opacity = 0
-  formContainer.style.transform = 'translateY(0%)'
+messageBtn.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+      formContainer.style.transform = 'translateY(0%)'
+      btn.style.transition = 'all 2s ease'
+      btn.style.transform = 'translateX(-250%)'
+      btn.style.opacity = 0 
+    })
 })
 window.addEventListener('load',()=>{
-  messageBtn.style.opacity = 1
   hero_p.style.transition = 'all 2s ease';
   hero_p.style.transform = 'translateY(0vh)'
   hero_big.style.transition = 'all 2s ease';
